bcd1c90d6c9130afc09b74a259755260265c2992
agasparovic0 <agasparovic@ffos.hr>
2025-02-21 11:18:44 +0100

    gfd

--- snippets/basic.py
+++ snippets/basic.py
@@ -1,35 +1,18 @@
+from pydantic import BaseModel
+
 from pydantic_ai import Agent
-<<<<<<< HEAD
-
-agent = Agent(  
-    'google-gla:gemini-1.5-flash',
-=======
-from dotenv import load_dotenv
-from httpx import AsyncClient
-import os
-from pydantic_ai.models.gemini import GeminiModel
 
 
-load_dotenv()
+class CityLocation(BaseModel):
+    city: str
+    country: str
 
 
-model = GeminiModel(
-    model_name='gemini-1.5-flash',
-    api_key=os.getenv('GEMINI_API_KEY'),
-    http_client=AsyncClient(),
-)
-
-agent = Agent(  
-    model,
->>>>>>> 51fa1648620b22abc7c2819e43ec40e378e7b1db
-    system_prompt='Be concise, reply with one sentence.',  
-)
-
-result = agent.run_sync('Where does "hello world" come from?')  
-<<<<<<< HEAD
+agent = Agent('google-gla:gemini-1.5-flash', result_type=CityLocation)
+result = agent.run_sync('Where were the olympics held in 2012?')
 print(result.data)
-=======
-print(result.data)
-
-
->>>>>>> 51fa1648620b22abc7c2819e43ec40e378e7b1db
+#> city='London' country='United Kingdom'
+print(result.usage())
+"""
+Usage(requests=1, request_tokens=57, response_tokens=8, total_tokens=65, details=None)
+"""
